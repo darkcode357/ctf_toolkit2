847212f4116592bc47ddd699b804e2c3d243ca6b
darkcode357 <darkcode357@gmail.com>
2018-02-19 14:28:49 -0300

    test setup
    Signed-off-by: darkcode357 <darkcode357@gmail.com>

---
+++ ctf/setup/setup.py
@@ -0,0 +1,10 @@
+import os
+def cria_urls_txt():
+    with open("urls.txt","w") as fl:
+        urls = ["http://scrapmaker.com/data/wordlists/dictionaries/rockyou.txt",'dsa','dd']
+        for i in urls:
+            if i == urls:
+                pass
+            else:
+                fl.write("\n"+i)
+creat()
