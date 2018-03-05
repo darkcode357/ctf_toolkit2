0573c43b7bfe1e5a50f2f17079ebe25cc856fa31
darkcode357 <darkcode357@gmail.com>
2018-03-05 13:44:13 -0500

    menu atualizado , agora so falta criar uma vertente do wget para baixar arquivos

--- ctf/menu/menu.py
+++ ctf/menu/menu.py
@@ -1,137 +1,213 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-cyanClaro = "\033[1;36m"
-vermelho = '\033[31;1m'
-verde = '\033[32;1m'
-azul = '\033[34;1m'
-normal = '\033[0;0m'
-amarelo = '\033[1;33m'
-ciano = '\033[46m'
-magenta = '\033[45m'
-# sair
-# reverse_shell
-# wordlist
-# decode
-# encode
-# ssh
-# ftp
-# documentacao do s modulos
-# documentacao do s modulos
-# decode
-# encode
-# imports
-# wordlist
-# ftp
-# ssh
-# imports
-# sair
-
-# wordlist
-# decode
-# encode
-# ssh
-# ftp
-# documentacao do s modulos
-# documentacao do s modulos
-# decode
-# encode
-# imports
-# wordlist
-# ftp
-# ssh
-# prompt
+# import libs_python
 import cmd
 
+from .modulos.base.decode.decode16.decode16 import decode16
+from .modulos.base.decode.decode32.decode32 import decode32
+# decode(base)
+from .modulos.base.decode.decode64.decode64 import decode64
+# encodes(base)
+from .modulos.base.encode.encode16 import encode16
+from .modulos.base.encode.encode32 import encode32
+from .modulos.base.encode.encode64 import encode64
+# database
+# connect
+# hex
+# scanner
+# kernel_exploit
+# web
 # donwload
-# imports
-# gera_payloads
-# reverse_shell
+from .modulos.donwloads.baixar import baixarU
+# gera_payload
+from .modulos.gera_payloads.gera_payloads import payloads
+from .modulos.hash.hashs import blake2b
+from .modulos.hash.hashs import md5
+from .modulos.hash.hashs import sha1
+from .modulos.hash.hashs import sha224
+from .modulos.hash.hashs import sha256
+from .modulos.hash.hashs import sha384
+from .modulos.hash.hashs import sha3_224
+# encode(hash)
+from .modulos.hash.hashs import sha3_256
+from .modulos.hash.hashs import sha3_384
+from .modulos.hash.hashs import sha3_512
+from .modulos.hash.hashs import sha512
 
-class ctf_promt(cmd.Cmd):
-    """ctf_promt"""
-    comandos = []
+
+# wordlist_gene
 
 
-'''
-def menu():
-    while True:
-        menu = prompt('ctf@toolkit=>: ', completer=animal_completer,
-                      key_bindings_registry=registry,
+class cores:
+    cyanClaro = "\033[1;36m"
+    vermelho = '\033[31;1m'
+    verde = '\033[32;1m'
+    azul = '\033[34;1m'
+    normal = '\033[0;0m'
+    amarelo = '\033[1;33m'
+    ciano = '\033[46m'
+    magenta = '\033[45m'
 
-                      # Important: for this to work: `complete_while_typing` needs
-                      #            to be False.
-                      complete_while_typing=True, style=example_style)
+
+while True:
+    try:
         try:
-            try:
-                if menu == "help use":
-                    print("%s[+]%s use comandos" % (amarelo, azul))
-                elif menu == "list":
-                    list()
-                elif menu == "clean":
-                    import os;
+            class ctf_promt(cmd.Cmd):
+                """
+            ██████╗ ██████╗  ██████╗ ███╗   ███╗██████╗ ████████╗      ██████╗████████╗ ██████╗
+            ██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██╔══██╗╚══██╔══╝     ██╔════╝╚══██╔══╝██╔════╝
+            ██████╔╝██████╔╝██║   ██║██╔████╔██║██████╔╝   ██║        ██║        ██║   ██║  ███╗
+            ██╔═══╝ ██╔══██╗██║   ██║██║╚██╔╝██║██╔═══╝    ██║        ██║        ██║   ██║   ██║
+            ██║     ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║        ██║███████╗╚██████╗   ██║   ╚██████╔╝
+            ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝        ╚═╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝
 
-                    os.system("clear")
-                    list()
-                    del os
-                elif menu == "use decode64":
-                    decode64()
-                elif menu == "use encode64":
-                    encode64()
-                elif menu == "use decode32":
-                    decode32()
-                elif menu == "use encode32":
-                    encode32()
-                elif menu == "use decode16":
-                    decode16()
-                elif menu == "use encode16":
-                    encode16()
-                elif menu == "use ssh":
-                    ssh()
-                elif menu == "use ftp":
-                    ftp()
-                elif menu == "use gera_wordlist":
-                    gera_wrdlist()
-                elif menu == "comandos":
-                    list()
-                elif menu == "info base64":
-                    help_base64()
-                elif menu == "use reverse_tcp":
-                    main()
-                elif menu == "use baixar":
-                    baixarU()
-                elif menu == "use gera_payload":
-                    payloads()
-                elif menu == 'use encode_sha3_256':
-                    sha3_256()
-                elif menu == 'use encode_sha256':
-                    sha256()
-                elif menu == 'use encode_blake2b':
-                    blake2b()
-                elif menu == 'use encode_sha384':
-                    sha384()
-                elif menu == 'use encode_md5':
-                    md5()
-                elif menu == 'use encode_sha3_512':
-                    sha3_512()
-                elif menu == 'use encode_sha512':
-                    sha512()
-                elif menu == 'use encode_sha1':
-                    sha1()
-                elif menu == 'use encode_sha3_224':
-                    sha3_224()
-                elif menu == 'use encode_blake2s':
-                    blake2s()
-                elif menu == 'use encode_sha3_384':
-                    sha3_384()
-                elif menu == 'use encode_sha224':
-                    sha224
-                elif menu == "sair":
-                    print("%s[+]%sobrigado por usar o ctf_toolkit" % (amarelo, azul))
-                    print("%s[+]%scriador darkcode" % (amarelo, azul))
+                """
+
+                prompt = cores.azul + 'ctf@toolkit=>: ' + cores.vermelho
+                intro = cores.amarelo + """bem vindo ao custon_pront_ctf_toolkit """
+
+                doc_header = cores.vermelho + '[+]lista de todos os comandos[+]'
+                misc_header = 'misc_header'
+                undoc_header = cores.azul + '[+]comando de saida[+]'
+
+                ruler = '-'
+
+                def do_prompt(self, line):
+                    "Change the interactive prompt"
+                    self.prompt = line + ': '
+
+                comandos = ['clear',
+                            'decode64',
+                            'decode32',
+                            'decode16',
+                            'encode64',
+                            'encode32',
+                            'encode16',
+                            'ssh',
+                            'ftp',
+                            'gera_wordlist',
+                            'reverse_tcp',
+                            'baixar',
+                            'gera_payload',
+                            'encode_sha3_256',
+                            'encode_sha256',
+                            'encode_blake2b',
+                            'encode_sha384',
+                            'encode_md5',
+                            'encode_sha3_512',
+                            'encode_sha512',
+                            'encode_sha1',
+                            'encode_sha3_224',
+                            'encode_blake2s',
+                            'encode_sha3_384',
+                            'encode_sha224',
+                            'sair',
+                            ]
+
+                def do_use(self, comando):
+                    "comandos"
+                    if comando and comando in self.comandos:
+                        if comando == "encode16":
+                            encode16()
+                        elif comando == "encode32":
+                            encode32()
+                        elif comando == "encode64":
+                            encode64()
+                        elif comando == "decode16":
+                            decode16()
+                        elif comando == "decode32":
+                            decode32()
+                        elif comando == "decode64":
+                            decode64()
+                        elif comando == "baixar":
+                            baixarU()
+                        elif comando == "gera_payload":
+                            payloads()
+                        elif comando == "encode_sha3_256":
+                            sha3_256()
+                        elif comando == "encode_sha256":
+                            sha256()
+                        elif comando == "encode_blake2b":
+                            blake2b()
+                        elif comando == "encode_sha384":
+                            sha384()
+                        elif comando == "encode_md5":
+                            md5()
+                        elif comando == "encode_sha3_512":
+                            sha3_512()
+                        elif comando == "encode_sha512":
+                            sha512()
+                        elif comando == "encode_sha1":
+                            sha1()
+                        elif comando == "encode_sha3_224":
+                            sha3_224()
+                        elif comando == "encode_sha3_384":
+                            sha3_384()
+                        elif comando == "encode_sha224":
+                            sha224()
+                        elif comando == "clear":
+                            from os import system as sys
+                            sys("clear")
+
+                    elif comando:
+                        saida = "[+]comando incompleto=> [%s]" % comando
+                    elif comando:
+                        saida = "[+]comando incompleto=> [%s]" % comando
+                    else:
+                        saida = '[+]comando errado...'
+
+                def complete_use(self, text, line, begidx, endidx):
+                    # completa os comandos
+                    if not text:
+                        completions = self.comandos[:]
+                    else:
+                        completions = [f
+                                       for f in self.comandos
+                                       if f.startswith(text)
+                                       ]
+                    return completions
+
+                def help_use(self):
+                    print('\n'.join([cores.amarelo + "[+]use $modulo" + cores.verde]))
+                    comandos = ['clear',
+                                'use decode64',
+                                'use decode32',
+                                'use decode16',
+                                'use encode64',
+                                'use encode32',
+                                'use encode16',
+                                'use ssh',
+                                'use ftp',
+                                'use gera_wordlist',
+                                'use comandos',
+                                'use reverse_tcp',
+                                'use baixar',
+                                'use gera_payload',
+                                'use encode_sha3_256',
+                                'use encode_sha256',
+                                'use encode_blake2b',
+                                'use encode_sha384',
+                                'use encode_md5',
+                                'use encode_sha3_512',
+                                'use encode_sha512',
+                                'use encode_sha1',
+                                'use encode_sha3_224',
+                                'use encode_blake2s',
+                                'use encode_sha3_384',
+                                'use encode_sha224',
+                                'sair',
+                                ]
+                    for i in comandos:
+                        print(i)
+
+                def do_sair(self, line):
+                    print("[+]obrigado por usar o ctf_toolkit")
                     exit(1)
-            except EOFError:
-                print(amarelo + "digite help")
+                    return True
 
-        except KeyboardInterrupt as e:
-            print(amarelo + "digite help")
-            '''
+
+            ctf_promt().cmdloop()
+        except EOFError:
+            print("[+]digite sair")
+    except KeyboardInterrupt:
+        print("[+]digite sair......")
---
+++ ctf/menu/modulos/word_list_generate/str_up_especial/str_up_especial.py
@@ -0,0 +1,22 @@
+import errno
+import itertools
+import os
+
+
+def str_up_especial():
+    try:
+        os.makedirs("../../../../files/wordlist/str_up_especial/")
+        print("[criando pasta wordlist/str_up_especial][OK]")
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+        pass
+    word_list_name = input("[+]nome da wordlist=>")
+    min = input("minimo de caractere=>[" + "]")
+    max = input("maximo de caractere=>[" + "]")
+    chrs = 'abcdefghijklmnopqrstuvwxyz~@#$%^&*()_+=-][}{\|""/?.><,,'.upper()
+    with open("../../../../files/wordlist/str_up_especial/" + word_list_name, "w") as fl:
+        min_length, max_length = int(min), int(max)
+        for n in range(min_length, max_length + 1):
+            for xs in itertools.product(chrs, repeat=n):
+                dsa = fl.write(''.join(xs) + "\n")
